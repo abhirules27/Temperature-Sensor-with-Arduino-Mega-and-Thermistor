2555eb52b756b5075ba8080e31fcd9a3c60e50d8
abhirules27 <abhirules27@gmail.com>
2016-11-11 20:09:58 +0530

    Add files via upload

---
+++ TempSensor_Thermistor.ino
@@ -0,0 +1,25 @@
+#include <math.h>
+
+double Thermistor(int Bravo) {
+ double Alpha;
+ Alpha = log(10000.0*((1024.0/Bravo-1))); 
+ Alpha = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Alpha * Alpha ))* Alpha );
+ Alpha = Alpha - 273.15;          
+ return Alpha;
+}
+
+void setup() {
+ Serial.begin(9600);
+}
+
+void loop() {             
+  int val;                
+  double Alpha;            
+  val=analogRead(0);      
+  Alpha=Thermistor(val);   
+  Serial.print("Temperature = ");
+  Serial.print(Alpha);   
+  Serial.println(" C");
+  delay(1000);            
+}
+
